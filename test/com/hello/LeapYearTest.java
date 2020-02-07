52ef45898b41fa83a98490c57cc545317e852ebe
Sanket Sanath <sanket@sigmoidanalytics.com>
2020-02-08 01:16:32 +0530

    add spy

--- test/com/hello/LeapYearTest.java
+++ test/com/hello/LeapYearTest.java
@@ -3,26 +3,35 @@ package com.hello;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Spy;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class LeapYearTest {
 
-    LeapYear leapYear;
+    LeapYear leapYear, sLeapYear;
 
     @Mock
     InputFromFile inputFromFile;
+
+    @Spy
+    InputFromFile sInputFromFile;
+
     @Before
     public void mockFun() {
         inputFromFile = mock(InputFromFile.class);
         leapYear = new LeapYear();
         leapYear.setInputFromFile(inputFromFile);
+
+        sInputFromFile = spy(new InputFromFile());
+        sLeapYear = new LeapYear();
+        sLeapYear.setInputFromFile(sInputFromFile);
+
     }
 
     @Test
@@ -33,55 +42,58 @@ public class LeapYearTest {
     }
 
     @Test
-    public void checkLeapYear() {
-        LeapYear checkLeapYear = new LeapYear();
-
-        boolean result = checkLeapYear.isLeapYear(2012);
-        assertTrue(result);
-
-        result = checkLeapYear.isLeapYear(1992);
-        assertTrue(result);
-    }
-
-    @Test
-    public void checkLeapYearOn100YearInterval() {
-        LeapYear checkLeapYear = new LeapYear();
-
-        boolean result = checkLeapYear.isLeapYear(2100);
-        assertFalse(result);
-
-        result = checkLeapYear.isLeapYear(1700);
-        assertFalse(result);
-
-        result = checkLeapYear.isLeapYear(1800);
-        assertFalse(result);
-
-        result = checkLeapYear.isLeapYear(1900);
+    public void testyearValueFromFile2() throws IOException {
+        doReturn(2002).when(sInputFromFile).getYear();
+        boolean result = leapYear.fun();
         assertFalse(result);
     }
 
     @Test
-    public void checkLeapYearOn400YearInterval() {
-        LeapYear checkLeapYear = new LeapYear();
+    public void leapYear() {
 
-        boolean result = checkLeapYear.isLeapYear(2000);
+        boolean result = leapYear.isLeapYear(2012);
         assertTrue(result);
 
-        result = checkLeapYear.isLeapYear(1600);
+        result = leapYear.isLeapYear(1992);
+        assertTrue(result);
+    }
+
+    @Test
+    public void leapYearOn100YearInterval() {
+
+        boolean result = leapYear.isLeapYear(2100);
+        assertFalse(result);
+
+        result = leapYear.isLeapYear(1700);
+        assertFalse(result);
+
+        result = leapYear.isLeapYear(1800);
+        assertFalse(result);
+
+        result = leapYear.isLeapYear(1900);
+        assertFalse(result);
+    }
+
+    @Test
+    public void leapYearOn400YearInterval() {
+
+        boolean result = leapYear.isLeapYear(2000);
         assertTrue(result);
 
-        result = checkLeapYear.isLeapYear(2400);
+        result = leapYear.isLeapYear(1600);
+        assertTrue(result);
+
+        result = leapYear.isLeapYear(2400);
         assertTrue(result);
     }
 
     @Test
     public void checkNotLeapYear() {
-        LeapYear checkLeapYear = new LeapYear();
 
-        boolean result = checkLeapYear.isLeapYear(2019);
+        boolean result = leapYear.isLeapYear(2019);
         assertFalse(result);
 
-        result = checkLeapYear.isLeapYear(2013);
+        result = leapYear.isLeapYear(2013);
         assertFalse(result);
     }
 }
